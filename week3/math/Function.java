95809f5116c94e73ea2068cefa4dd944a8e32ead
marcpichel <m.p.pichel@alumnus.utwente.nl>
2018-12-16 13:57:05 +0100

    function test

--- week3/math/Function.java
+++ week3/math/Function.java
@@ -1,3 +1,12 @@
 package ss.week3.math.Function
 
-public class  function
+private class function
+
+public static PARAM_A = 5;
+public static PARAM_B = 1;
+// Define function of x as Ax^2 + Bx^1 + Cx^0
+/@ JML test here
+public int G = f[x];
+public int x = i-10;
+
+// Begin derivative towards dx
